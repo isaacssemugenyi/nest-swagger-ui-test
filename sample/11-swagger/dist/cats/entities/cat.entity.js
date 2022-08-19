93f4546591c9fbbfbc100e35d0e045d1bc49a9ef
isaa ssemugenyi <isaacpro01@gmail.com>
2022-08-20 00:33:35 +0300

    All dist to be sent

---
+++ sample/11-swagger/dist/cats/entities/cat.entity.js
@@ -0,0 +1,32 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Cat = void 0;
+const openapi = require("@nestjs/swagger");
+const swagger_1 = require("@nestjs/swagger");
+class Cat {
+    static _OPENAPI_METADATA_FACTORY() {
+        return { name: { required: true, type: () => String, description: "The name of the Cat", example: "Kitty" }, age: { required: true, type: () => Number }, breed: { required: true, type: () => String } };
+    }
+}
+__decorate([
+    (0, swagger_1.ApiProperty)({ example: 1, description: 'The age of the Cat' }),
+    __metadata("design:type", Number)
+], Cat.prototype, "age", void 0);
+__decorate([
+    (0, swagger_1.ApiProperty)({
+        example: 'Maine Coon',
+        description: 'The breed of the Cat',
+    }),
+    __metadata("design:type", String)
+], Cat.prototype, "breed", void 0);
+exports.Cat = Cat;
+//# sourceMappingURL=cat.entity.js.map
---
+++ sample/11-swagger/dist/main.js
@@ -0,0 +1,21 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const core_1 = require("@nestjs/core");
+const swagger_1 = require("@nestjs/swagger");
+const app_module_1 = require("./app.module");
+async function bootstrap() {
+    const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    const options = new swagger_1.DocumentBuilder()
+        .setTitle('Cats example')
+        .setDescription('The cats API description')
+        .setVersion('1.0')
+        .addTag('cats')
+        .addBearerAuth()
+        .build();
+    const document = swagger_1.SwaggerModule.createDocument(app, options);
+    swagger_1.SwaggerModule.setup('api', app, document);
+    await app.listen(3000);
+    console.log(`Application is running on: ${await app.getUrl()}`);
+}
+bootstrap();
+//# sourceMappingURL=main.js.map
